e1ea735ae352d4f798a514ca15cbc980760ecec3
alfalfascout <alfalfascouting@gmail.com>
2016-06-20 01:29:10 -0400

    tabs to spaces

--- src/main/java/com/alfalfascout/CustomVillageTrades/CustomVillageTrades.java
+++ src/main/java/com/alfalfascout/CustomVillageTrades/CustomVillageTrades.java
@@ -19,67 +19,67 @@ import org.bukkit.inventory.MerchantRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomVillageTrades extends JavaPlugin implements Listener {
-	Random rand = new Random();
-	Material currency = Material.EMERALD;
-	boolean vanilla_trades;
-	private File configf, librariansf, vanillaf;
+    Random rand = new Random();
+    Material currency = Material.EMERALD;
+    boolean vanilla_trades;
+    private File configf, librariansf, vanillaf;
     private FileConfiguration config, librarians, vanilla;
-	
+    
     public void onEnable() {
-		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-		createFiles();
-		getDefaultConfigs();
-		getLogger().info("Librarians: " +
-				Boolean.toString(librarians.contains("id0")));
-	}
-	
-	public void onDisable() {
-		try {
-			librarians.save(librariansf);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			config.save(configf);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			vanilla.save(vanillaf);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public FileConfiguration getLibrarians() {
-		return this.librarians;
-	}
-	
-	public FileConfiguration getVanilla() {
-		return this.vanilla;
-	}
-	
-	public void createFiles() {
-		File configf = new File(getDataFolder(), "config.yml");
-		File librariansf = new File(getDataFolder(), "librarians.yml");
-		File vanillaf = new File(getDataFolder(), "vanilla_trades.yml");
-		if (!configf.exists()) {
-		    getLogger().info("config.yml not found, creating!");
-		    configf.getParentFile().mkdirs();
-		    saveResource("config.yml", false);
-		}
-		if (!librariansf.exists()) {
-		    getLogger().info("librarians.yml not found, creating!");
-		    librariansf.getParentFile().mkdirs();
-		    saveResource("librarians.yml", false);
-		}
-		if (!vanillaf.exists()) {
-			getLogger().info("vanilla_trades.yml not found, creating!");
-			vanillaf.getParentFile().mkdirs();
-			saveResource("vanilla_trades.yml", false);
-		}
-		
-		YamlConfiguration config = new YamlConfiguration();
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        createFiles();
+        getDefaultConfigs();
+        getLogger().info("Librarians: " +
+                Boolean.toString(librarians.contains("id0")));
+    }
+    
+    public void onDisable() {
+        try {
+            librarians.save(librariansf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            config.save(configf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            vanilla.save(vanillaf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public FileConfiguration getLibrarians() {
+        return this.librarians;
+    }
+    
+    public FileConfiguration getVanilla() {
+        return this.vanilla;
+    }
+    
+    public void createFiles() {
+        File configf = new File(getDataFolder(), "config.yml");
+        File librariansf = new File(getDataFolder(), "librarians.yml");
+        File vanillaf = new File(getDataFolder(), "vanilla_trades.yml");
+        if (!configf.exists()) {
+            getLogger().info("config.yml not found, creating!");
+            configf.getParentFile().mkdirs();
+            saveResource("config.yml", false);
+        }
+        if (!librariansf.exists()) {
+            getLogger().info("librarians.yml not found, creating!");
+            librariansf.getParentFile().mkdirs();
+            saveResource("librarians.yml", false);
+        }
+        if (!vanillaf.exists()) {
+            getLogger().info("vanilla_trades.yml not found, creating!");
+            vanillaf.getParentFile().mkdirs();
+            saveResource("vanilla_trades.yml", false);
+        }
+        
+        YamlConfiguration config = new YamlConfiguration();
         YamlConfiguration librarians = new YamlConfiguration();
         YamlConfiguration vanilla = new YamlConfiguration();
         
@@ -91,180 +91,180 @@ public class CustomVillageTrades extends JavaPlugin implements Listener {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InvalidConfigurationException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void getDefaultConfigs() {
-		if (getConfig().contains("currency")) {
-			currency = Material.matchMaterial(
-					getConfig().getString("currency"));
-			if (currency == null) {
-				getLogger().warning("No material found matching '" + 
-						getConfig().getString("currency") + "' at currency. " +
-						"Using default.");
-				currency = Material.EMERALD;
-			}
-		}
-		else {
-			getLogger().info("Config has no currency! Adding default.");
-			config.createSection("currency");
-			config.set("currency", "emerald");
-		}
-		getLogger().info("Currency is " + currency.toString());
-		
-		if (getConfig().contains("allow_vanilla_trades")) {
-			try {
-				vanilla_trades = getConfig().getBoolean("allow_vanilla_trades");
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-				getLogger().warning("Value at allow_vanilla_trades " +
-						"should be true or false.");
-			}
-		}
-	}
-	
-	@EventHandler
-	public void onTradeAcquire(VillagerAcquireTradeEvent e) {
-		Villager villager = e.getEntity();
-		MerchantRecipe recipe = e.getRecipe();
-		
-		CareerTier trade = new CareerTier(this);
-		CareerTier.setCareerTier(trade, villager, recipe);
-		
-		if (trade.tier > 0) {
-			String path = trade.career + ".tier" + 
-						Integer.toString(trade.tier);
-			List<MerchantRecipe> new_trades = getTradesInTier(path);
-			
-			for (MerchantRecipe new_trade : new_trades) {
-				addRecipe(villager, new_trade);
-			}
-		}
-		
-		e.setCancelled(!vanilla_trades);
-	}
-		
-	public void addRecipe(Villager villager, MerchantRecipe recipe) {
-		List<MerchantRecipe> newrecipes = new ArrayList<MerchantRecipe>();
-		for (int i = 0; i < villager.getRecipeCount(); i++) {
-			newrecipes.add(villager.getRecipe(i));
-		}
-		newrecipes.add(recipe);
-		villager.setRecipes(newrecipes);
-	}
-	
-	public List<MerchantRecipe> getTradesInTier(String path) {
-		List<MerchantRecipe> list = new ArrayList<MerchantRecipe>();
-		
-		for (String new_trade : getConfig().getStringList(path)) {
-			String trade_path = path + "." + new_trade;
-			
-			ItemStack result = new ItemStack(getItemInTrade(
-					trade_path + ".result"));
-			List<ItemStack> ingredients = new ArrayList<ItemStack>();
-			ingredients.add(new ItemStack(getItemInTrade(
-					trade_path + ".ingredient1")));
-			if (getConfig().contains(trade_path + ".ingredient2")) {
-				ingredients.add(new ItemStack(getItemInTrade(
-						trade_path + ".ingredient2")));
-			}
-			
-			list.add(new MerchantRecipe(result, 7));
-			
-			for (ItemStack ingredient : ingredients) {
-				list.get(list.size() - 1).addIngredient(ingredient);
-			}
-		}
-		
-		return list;
-	}
-	
-	public ItemStack getItemInTrade(String path) {
-		
-		String material_name = getConfig().getString(path + ".material");
-		Material item_type;
-		if (material_name == "currency") {
-			item_type = currency;
-		}
-		else {
-			item_type = Material.matchMaterial(material_name);
-			if (item_type == null) {
-				item_type = Material.AIR;
-				getLogger().warning("No material matching '" + 
-						material_name + "'. " + path);
-			}
-		}
-		
-		ItemStack item = new ItemStack(item_type);
-		
-		if (getConfig().contains(path + ".min") &&
-				getConfig().contains(path + ".max")) {
-			try {
-				int min = getConfig().getInt(path + ".min");
-				int max = getConfig().getInt(path + ".max") - min;
-				item.setAmount(rand.nextInt(min) + max); 
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-				getLogger().warning("The value in " + path +
-						".min or .max should be an integer.");
-			}
-		}
-		
-		if (getConfig().contains(path + ".data")) {
-			try {
-				item.setDurability(
-						(short)getConfig().getInt(path + ".data"));
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-				getLogger().warning("The value in " + path + 
-						".data should be an integer.");
-			}
-			
-		}
-		
-		if (getConfig().contains(path + ".enchantment")) {
-			int level = 1;
-			boolean allow_treasure = false;
-			
-			if (getConfig().contains(path + ".enchantment.level")) {
-				try {
-					level = getConfig().getInt(path + ".enchantment.level");
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-					getLogger().warning("The value in " + path + 
-							".enchantment.level should be an integer.");
-				}
-			}
-			
-			if (getConfig().contains(path + ".enchantment.allow_treasure")) {
-				try {
-					allow_treasure = getConfig().getBoolean(path
-							+ ".enchantment.allow_treasure");
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-					getLogger().warning("The value in " + path + 
-							".enchantment.allow_treasure should be " +
-							"true or false.");
-				}
-			}
-			
-			if (item.getType().equals(Material.ENCHANTED_BOOK) ||
-					item.getType().equals(Material.BOOK)) {
-				item = EnchantHelper.randomEnchantedBook(
-						level, allow_treasure);
-			}
-			else {
-				item = EnchantHelper.randomEnchantment(
-						item, level, allow_treasure);
-			}
-		}
-		return item;
-	}
-	
+            e.printStackTrace();
+        }
+    }
+    
+    public void getDefaultConfigs() {
+        if (getConfig().contains("currency")) {
+            currency = Material.matchMaterial(
+                    getConfig().getString("currency"));
+            if (currency == null) {
+                getLogger().warning("No material found matching '" + 
+                        getConfig().getString("currency") + "' at currency. " +
+                        "Using default.");
+                currency = Material.EMERALD;
+            }
+        }
+        else {
+            getLogger().info("Config has no currency! Adding default.");
+            config.createSection("currency");
+            config.set("currency", "emerald");
+        }
+        getLogger().info("Currency is " + currency.toString());
+        
+        if (getConfig().contains("allow_vanilla_trades")) {
+            try {
+                vanilla_trades = getConfig().getBoolean("allow_vanilla_trades");
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                getLogger().warning("Value at allow_vanilla_trades " +
+                        "should be true or false.");
+            }
+        }
+    }
+    
+    @EventHandler
+    public void onTradeAcquire(VillagerAcquireTradeEvent e) {
+        Villager villager = e.getEntity();
+        MerchantRecipe recipe = e.getRecipe();
+        
+        CareerTier trade = new CareerTier(this);
+        CareerTier.setCareerTier(trade, villager, recipe);
+        
+        if (trade.tier > 0) {
+            String path = trade.career + ".tier" + 
+                        Integer.toString(trade.tier);
+            List<MerchantRecipe> new_trades = getTradesInTier(path);
+            
+            for (MerchantRecipe new_trade : new_trades) {
+                addRecipe(villager, new_trade);
+            }
+        }
+        
+        e.setCancelled(!vanilla_trades);
+    }
+        
+    public void addRecipe(Villager villager, MerchantRecipe recipe) {
+        List<MerchantRecipe> newrecipes = new ArrayList<MerchantRecipe>();
+        for (int i = 0; i < villager.getRecipeCount(); i++) {
+            newrecipes.add(villager.getRecipe(i));
+        }
+        newrecipes.add(recipe);
+        villager.setRecipes(newrecipes);
+    }
+    
+    public List<MerchantRecipe> getTradesInTier(String path) {
+        List<MerchantRecipe> list = new ArrayList<MerchantRecipe>();
+        
+        for (String new_trade : getConfig().getStringList(path)) {
+            String trade_path = path + "." + new_trade;
+            
+            ItemStack result = new ItemStack(getItemInTrade(
+                    trade_path + ".result"));
+            List<ItemStack> ingredients = new ArrayList<ItemStack>();
+            ingredients.add(new ItemStack(getItemInTrade(
+                    trade_path + ".ingredient1")));
+            if (getConfig().contains(trade_path + ".ingredient2")) {
+                ingredients.add(new ItemStack(getItemInTrade(
+                        trade_path + ".ingredient2")));
+            }
+            
+            list.add(new MerchantRecipe(result, 7));
+            
+            for (ItemStack ingredient : ingredients) {
+                list.get(list.size() - 1).addIngredient(ingredient);
+            }
+        }
+        
+        return list;
+    }
+    
+    public ItemStack getItemInTrade(String path) {
+        
+        String material_name = getConfig().getString(path + ".material");
+        Material item_type;
+        if (material_name == "currency") {
+            item_type = currency;
+        }
+        else {
+            item_type = Material.matchMaterial(material_name);
+            if (item_type == null) {
+                item_type = Material.AIR;
+                getLogger().warning("No material matching '" + 
+                        material_name + "'. " + path);
+            }
+        }
+        
+        ItemStack item = new ItemStack(item_type);
+        
+        if (getConfig().contains(path + ".min") &&
+                getConfig().contains(path + ".max")) {
+            try {
+                int min = getConfig().getInt(path + ".min");
+                int max = getConfig().getInt(path + ".max") - min;
+                item.setAmount(rand.nextInt(min) + max); 
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                getLogger().warning("The value in " + path +
+                        ".min or .max should be an integer.");
+            }
+        }
+        
+        if (getConfig().contains(path + ".data")) {
+            try {
+                item.setDurability(
+                        (short)getConfig().getInt(path + ".data"));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                getLogger().warning("The value in " + path + 
+                        ".data should be an integer.");
+            }
+            
+        }
+        
+        if (getConfig().contains(path + ".enchantment")) {
+            int level = 1;
+            boolean allow_treasure = false;
+            
+            if (getConfig().contains(path + ".enchantment.level")) {
+                try {
+                    level = getConfig().getInt(path + ".enchantment.level");
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    getLogger().warning("The value in " + path + 
+                            ".enchantment.level should be an integer.");
+                }
+            }
+            
+            if (getConfig().contains(path + ".enchantment.allow_treasure")) {
+                try {
+                    allow_treasure = getConfig().getBoolean(path
+                            + ".enchantment.allow_treasure");
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    getLogger().warning("The value in " + path + 
+                            ".enchantment.allow_treasure should be " +
+                            "true or false.");
+                }
+            }
+            
+            if (item.getType().equals(Material.ENCHANTED_BOOK) ||
+                    item.getType().equals(Material.BOOK)) {
+                item = EnchantHelper.randomEnchantedBook(
+                        level, allow_treasure);
+            }
+            else {
+                item = EnchantHelper.randomEnchantment(
+                        item, level, allow_treasure);
+            }
+        }
+        return item;
+    }
+    
 }
--- src/main/java/com/alfalfascout/CustomVillageTrades/LeveledEnchantment.java
+++ src/main/java/com/alfalfascout/CustomVillageTrades/LeveledEnchantment.java
@@ -5,69 +5,69 @@ import java.util.List;
 import java.util.Random;
 
 public class LeveledEnchantment extends EnchantmentWrapper {
-	private int level;
-	private int weight;
-	
-	public LeveledEnchantment(int id, int level) {
-		super(id);
-		this.level = level;
-		this.setWeight(id);
-	}
-	
-	public int getLevel() {
-		return this.level;
-	}
+    private int level;
+    private int weight;
+    
+    public LeveledEnchantment(int id, int level) {
+        super(id);
+        this.level = level;
+        this.setWeight(id);
+    }
+    
+    public int getLevel() {
+        return this.level;
+    }
 
-	public int getWeight() {
-		return weight;
-	}
+    public int getWeight() {
+        return weight;
+    }
 
-	public void setWeight(int id) {
-		switch (id) {
-			case 0: case 16: case 32: case 48:
-				this.weight = 10;
-				break;
-			case 1: case 2: case 4: case 17: case 18: case 19:
-			case 34:
-				this.weight = 5;
-				break;
-			case 3: case 5: case 6: case 8: case 9: case 20:
-			case 21: case 35: case 49: case 50: case 61:
-			case 62: case 70:
-				this.weight = 2;
-				break;
-			case 7: case 33: case 51:
-				this.weight = 1;
-				break;
-			default:
-				this.weight = 2;
-		}
-	}
-	
-	public static LeveledEnchantment getRandomLeveledEnchant(
-			List<LeveledEnchantment> list, Random rand) {
-		LeveledEnchantment chosenEnchant = new LeveledEnchantment(0, 0);
-		int totalWeight = 0;
-		int i = 0;
-		int j;
-		
-		for (j = list.size(); i < j; ++i) {
-			LeveledEnchantment enchantment = list.get(i);
-			totalWeight += enchantment.weight;
-		}
-		
-		int k = rand.nextInt(totalWeight);
-		i = 0;
-		
-		for (j = list.size(); i < j; ++i) {
-			chosenEnchant = list.get(i);
-			k -= chosenEnchant.weight;
-			
-			if (k < 0) {
-				return chosenEnchant;
-			}
-		}
-		
-		return (LeveledEnchantment)null;
-	}
+    public void setWeight(int id) {
+        switch (id) {
+            case 0: case 16: case 32: case 48:
+                this.weight = 10;
+                break;
+            case 1: case 2: case 4: case 17: case 18: case 19:
+            case 34:
+                this.weight = 5;
+                break;
+            case 3: case 5: case 6: case 8: case 9: case 20:
+            case 21: case 35: case 49: case 50: case 61:
+            case 62: case 70:
+                this.weight = 2;
+                break;
+            case 7: case 33: case 51:
+                this.weight = 1;
+                break;
+            default:
+                this.weight = 2;
+        }
+    }
+    
+    public static LeveledEnchantment getRandomLeveledEnchant(
+            List<LeveledEnchantment> list, Random rand) {
+        LeveledEnchantment chosenEnchant = new LeveledEnchantment(0, 0);
+        int totalWeight = 0;
+        int i = 0;
+        int j;
+        
+        for (j = list.size(); i < j; ++i) {
+            LeveledEnchantment enchantment = list.get(i);
+            totalWeight += enchantment.weight;
+        }
+        
+        int k = rand.nextInt(totalWeight);
+        i = 0;
+        
+        for (j = list.size(); i < j; ++i) {
+            chosenEnchant = list.get(i);
+            k -= chosenEnchant.weight;
+            
+            if (k < 0) {
+                return chosenEnchant;
+            }
+        }
+        
+        return (LeveledEnchantment)null;
+    }
 }
