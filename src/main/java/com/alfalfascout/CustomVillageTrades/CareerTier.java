a8fe23d30d31f175158a868861e6e81b641c691f
Alan Jacon <alfalfascouting@gmail.com>
2016-07-13 07:10:14 -0400

    Revert "first try at overwriting all villagers"
    This reverts commit b74464bb552e7978dbeeebb94f77080a7b8dc4fd.

--- src/main/java/com/alfalfascout/CustomVillageTrades/CareerTier.java
+++ src/main/java/com/alfalfascout/CustomVillageTrades/CareerTier.java
@@ -188,14 +188,14 @@ public class CareerTier {
         int last = 0;
         
         //check the librarian file
-        String villagerId = "id" + Integer.toString(villager.getEntityId());
-        if (plugin.getVillagers().contains(villagerId)) {
-            last = plugin.getVillagers().getInt(villagerId);
+        String villager_id = "id" + Integer.toString(villager.getEntityId());
+        if (plugin.getVillagers().contains(villager_id)) {
+            last = plugin.getVillagers().getInt(villager_id);
         }
         //check librarian tier list against their trade list
         else if (plugin.getConfig().contains("librarian")) {
             List<Integer> tradesByTier = new ArrayList<Integer>();
-            int currentTrades = villager.getRecipeCount();
+            int currenttrades = villager.getRecipeCount();
             
             if (plugin.getConfig().getString("librarian") != "default") {
                 for (String tier : plugin.getConfig().getStringList(
@@ -213,7 +213,7 @@ public class CareerTier {
                 }
             }
             for (int trades : tradesByTier) {
-                if (currentTrades < trades) {
+                if (currenttrades < trades) {
                     last = tradesByTier.indexOf(trades);
                 }
             }
@@ -226,11 +226,11 @@ public class CareerTier {
     }
     
     public static void saveVillager(CareerTier careerTier, Villager villager) {
-        String villagerId = "id" + Integer.toString(villager.getEntityId());
-        if (!plugin.getVillagers().contains(villagerId)) {
-            plugin.getVillagers().createSection(villagerId);
+        String villager_id = "id" + Integer.toString(villager.getEntityId());
+        if (!plugin.getVillagers().contains(villager_id)) {
+            plugin.getVillagers().createSection(villager_id);
         }
         
-        plugin.getVillagers().set(villagerId, careerTier.tier);
+        plugin.getVillagers().set(villager_id, careerTier.tier);
     }
 }
